2e83794a6fa728559f29d2ec83e190ca05096fbc
Sayfullayev006 <153357600+Sayfullayev006@users.noreply.github.com>
2023-12-09 15:47:12 +0500

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,174 @@
+let movies = [
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/xgGGinKRL8xeRkaAR9RMbtyk60y.jpg",
+      genre_ids: [16, 10751, 10402, 14, 35],
+      id: 901362,
+      original_language: "en",
+      original_title: "Trolls Band Together",
+      overview:
+        "When Branch’s brother, Floyd, is kidnapped for his musical talents by a pair of nefarious pop-star villains, Branch and Poppy embark on a harrowing and emotional journey to reunite the other brothers and rescue Floyd from a fate even worse than pop-culture obscurity.",
+      popularity: 2508.728,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/bkpPTZUdq31UGDovmszsg2CchiI.jpg",
+      release_date: "2023-10-12",
+      title: "Trolls Band Together",
+      video: false,
+      vote_average: 7.3,
+      vote_count: 127,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/kjQBrc00fB2RjHZB3PGR4w9ibpz.jpg",
+      genre_ids: [878, 28, 53],
+      id: 670292,
+      original_language: "en",
+      original_title: "The Creator",
+      overview:
+        "Amid a future war between the human race and the forces of artificial intelligence, a hardened ex-special forces agent grieving the disappearance of his wife, is recruited to hunt down and kill the Creator, the elusive architect of advanced AI who has developed a mysterious weapon with the power to end the war—and mankind itself.",
+      popularity: 1622.502,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/vBZ0qvaRxqEhZwl6LWmruJqWE8Z.jpg",
+      release_date: "2023-09-27",
+      title: "The Creator",
+      video: false,
+      vote_average: 7.1,
+      vote_count: 1104,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg",
+      genre_ids: [18, 36],
+      id: 872585,
+      original_language: "en",
+      original_title: "Oppenheimer",
+      overview:
+        "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+      popularity: 1766.305,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+      release_date: "2023-07-19",
+      title: "Oppenheimer",
+      video: false,
+      vote_average: 8.2,
+      vote_count: 4885,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg",
+      genre_ids: [27, 9648],
+      id: 507089,
+      original_language: "en",
+      original_title: "Five Nights at Freddy's",
+      overview:
+        "Recently fired and desperate for work, a troubled young man named Mike agrees to take a position as a night security guard at an abandoned theme restaurant: Freddy Fazbear's Pizzeria. But he soon discovers that nothing at Freddy's is what it seems.",
+      popularity: 1252.998,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/j9mH1pr3IahtraTWxVEMANmPSGR.jpg",
+      release_date: "2023-10-25",
+      title: "Five Nights at Freddy's",
+      video: false,
+      vote_average: 7.9,
+      vote_count: 2501,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/28er4p7B5zMSxUDQKPF1hBsgnys.jpg",
+      genre_ids: [28, 12, 53],
+      id: 872906,
+      original_language: "hi",
+      original_title: "जवान",
+      overview:
+        "An emotional journey of a prison warden, driven by a personal vendetta while keeping up to a promise made years ago, recruits inmates to commit outrageous crimes that shed light on corruption and injustice, in an attempt to get even with his past,  and that leads him to an unexpected reunion.",
+      popularity: 903.515,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/jFt1gS4BGHlK8xt76Y81Alp4dbt.jpg",
+      release_date: "2023-09-07",
+      title: "Jawan",
+      video: false,
+      vote_average: 7.2,
+      vote_count: 113,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/rMvPXy8PUjj1o8o1pzgQbdNCsvj.jpg",
+      genre_ids: [28, 12, 53],
+      id: 299054,
+      original_language: "en",
+      original_title: "Expend4bles",
+      overview:
+        "Armed with every weapon they can get their hands on and the skills to use them, The Expendables are the world’s last line of defense and the team that gets called when all other options are off the table. But new team members with new styles and tactics are going to give “new blood” a whole new meaning.",
+      popularity: 873.341,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/iwsMu0ehRPbtaSxqiaUDQB9qMWT.jpg",
+      release_date: "2023-09-15",
+      title: "Expend4bles",
+      video: false,
+      vote_average: 6.4,
+      vote_count: 792,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/4XM8DUTQb3lhLemJC51Jx4a2EuA.jpg",
+      genre_ids: [28, 80, 53],
+      id: 385687,
+      original_language: "en",
+      original_title: "Fast X",
+      overview:
+        "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
+      popularity: 749.903,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/fiVW06jE7z9YnO4trhaMEdclSiC.jpg",
+      release_date: "2023-05-17",
+      title: "Fast X",
+      video: false,
+      vote_average: 7.2,
+      vote_count: 4334,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/628Dep6AxEtDxjZoGP78TsOxYbK.jpg",
+      genre_ids: [28, 53],
+      id: 575264,
+      original_language: "en",
+      original_title: "Mission: Impossible - Dead Reckoning Part One",
+      overview:
+        "Ethan Hunt and his IMF team embark on their most dangerous mission yet: To track down a terrifying new weapon that threatens all of humanity before it falls into the wrong hands. With control of the future and the world's fate at stake and dark forces from Ethan's past closing in, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan must consider that nothing can matter more than his mission—not even the lives of those he cares about most.",
+      popularity: 762.172,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/NNxYkU70HPurnNCSiCjYAmacwm.jpg",
+      release_date: "2023-07-08",
+      title: "Mission: Impossible - Dead Reckoning Part One",
+      video: false,
+      vote_average: 7.6,
+      vote_count: 2531,
+    },
+    {
+      adult: false,
+      backdrop_path:
+        "https://image.tmdb.org/t/p/w1280/5a4JdoFwll5DRtKMe7JLuGQ9yJm.jpg",
+      genre_ids: [28, 12, 878],
+      id: 695721,
+      original_language: "en",
+      original_title: "The Hunger Games: The Ballad of Songbirds & Snakes",
+      overview:
+        "64 years before he becomes the tyrannical president of Panem, Coriolanus Snow sees a chance for a change in fortunes when he mentors Lucy Gray Baird, the female tribute from District 12.",
+      popularity: 595.362,
+      poster_path:
+        "https://image.tmdb.org/t/p/w1280/mBaXZ95R2OxueZhvQbcEWy2DqyO.jpg",
+      release_date: "2023-11-15",
+      title: "The Hunger Games: The Ballad of Songbirds & Snakes",
+      video: false,
+      vote_average: 7.2,
+      vote_count: 392,
+    },
+  ];
+  
